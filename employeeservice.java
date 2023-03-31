cb2e58e630a785363cded91d76b6596d3a1d608b
Anilaws-devops <127336513+Anilaws-devops@users.noreply.github.com>
2023-03-31 10:09:33 +0530

    Update employeeservice.java

--- employeeservice.java
+++ employeeservice.java
@@ -1,3 +1,16 @@
 this file contain employee details ..
 updated employee deals
 thank for all 
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+  
