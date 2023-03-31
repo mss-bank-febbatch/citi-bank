46d6ff4161779e20daa94705a145ddaa82a578aa
Anilaws-devops <127336513+Anilaws-devops@users.noreply.github.com>
2023-03-31 10:44:06 +0530

    Update DButils.java

--- DButils.java
+++ DButils.java
@@ -1,2 +1,9 @@
 Hello Everyone
 git commands are very imp to developers to code
+
+
+num1 = int(input("Please Enter the First Number  = "))
+num2 = int(input("Please Enter the second number = "))
+
+sub = num1 - num2
+print('The Result of subtracting {0} from {1} = {2}'.format(num2,num1,sub))
